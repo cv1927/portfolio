947c891b3c5d5efc603c7ab89f261b7313491b56
Camilov1927 <camilov1927@gmail.com>
2019-08-08 23:12:22 -0500

    avance del diseño de pagina home con animaciones con gsap

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import { TimelineLite,Bounce } from 'gsap'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('title') title: ElementRef;
+  @ViewChild('subtitle') subtitle: ElementRef;
+  
+  timeLine:TimelineLite;
+
+  constructor() {
+    this.timeLine = new TimelineLite();
+  }
 
   ngOnInit() {
+    this.timeLine.to(this.title.nativeElement, 2, {opacity:0, y:10, ease: Bounce.easeOut});
+    this.timeLine.to(this.title.nativeElement, 1.5, {opacity:1, y:0, ease: Bounce.easeOut});
+    this.timeLine.to(this.subtitle.nativeElement, 1.5, {opacity:0, y:50, ease: Bounce.easeInOut})
+    this.timeLine.to(this.subtitle.nativeElement, 1.5, {opacity:1, y:0, ease: Bounce.easeOut})
   }
 
 }
